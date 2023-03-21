ee9d46a6f148375a71ce4097d0c0905a10c4e5cc
Rosa Tomalin <rosa.tomalin@gmail.com>
2023-03-21 09:57:17 +0000

    added random word function

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let randomWord = document.getElementById("random-word");
+let word = document.getElementById("random-word");
 let userGuesses = document.getElementById("user-guess");
 let usedLives = document.getElementById("used-lives");
 let button = document.getElementById("button");
 
 
 function appendGuess (param) {
+    // need to add a function checking if the guess is correct or not
     let newLetter = document.createElement('span');
     newLetter.textContent = `${param}, `;
     userGuesses.appendChild(newLetter);
 }
 
 function lifeCounter() {
-    // let usedUp =  document.createElement('span');
-    // usedUp.textContent = "X";
-    usedLives.removeChild(usedLives.lastElementChild);
+    let life = usedLives.getElementsByClassName("life")[0];
+    // need to add a function checking if the guess is correct or not
+    usedLives.removeChild(life);
 }
+
+// Getting user guess
 function getInput() {
     let userInput = document.getElementById("letter-input").value;
     if (userInput.length === 1) {
@@ -22,11 +25,25 @@ function getInput() {
         // need to change this so lifeCounter doesn't increase if guess is correct
         lifeCounter();
     } else {
+        // do something better than alert!
         alert("that is not a valid guess! Try again")
     }
 }
-    
-
 
 button.onclick = getInput;
+
+// function for picking random word (out of 2605)
+function randomWord() {
+
+    let randomNumber = Math.floor(Math.random() * 2605);
+    word.innerText = words[randomNumber - 1];
+}
+
+// call the random word function on page load
+
+randomWord();
+
+
+
+
 
